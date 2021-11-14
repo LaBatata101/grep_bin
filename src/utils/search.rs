5189601d6d3cdf0ea31a1c55564dd7230a9da8f3
LaBatata101 <labatata101@linuxmail.org>
2021-11-14 19:57:55 -0300

    refactor code and add comments

--- src/utils/search.rs
+++ src/utils/search.rs
@@ -3,100 +3,175 @@ use std::io::{BufReader, Read};
 use std::ops::Range;
 
 #[derive(Debug)]
-pub struct Match<'a> {
+pub struct Match {
     index: Range<usize>,
-    data: &'a [u8],
+    data: Vec<u8>,
+    offset: usize,
 }
 
-impl<'a> Match<'a> {
-    pub fn new(index: Range<usize>, data: &'a [u8]) -> Self {
-        Self { index, data }
+impl Match {
+    pub fn new(offset: usize, index: Range<usize>, data: Vec<u8>) -> Self {
+        Self {
+            index,
+            data,
+            offset,
+        }
+    }
+
+    pub fn offset(&self) -> usize {
+        self.offset
     }
 
     /// Get a reference to the match's index.
-    pub fn index(&self) -> &Range<usize> {
-        &self.index
+    pub fn index(&self) -> Range<usize> {
+        self.index.clone()
     }
 
     /// Get a reference to the match's data.
-    pub fn data(&self) -> &'a [u8] {
-        self.data
+    pub fn data(&self) -> &[u8] {
+        &self.data
     }
 }
 
-const BUFFER_SIZE: usize = 8192;
+pub struct Searcher<'a> {
+    pattern: &'a [u8],
+    result: Vec<Vec<Match>>,
+    context_bytes_size: usize,
+}
 
-pub fn search_in_file(pattern: &[u8], mut file: BufReader<File>) -> Vec<Vec<Match>> {
-    let mut buffer = [0; BUFFER_SIZE];
-    let mut matches = Vec::new();
-    loop {
-        let n = file.read(&mut buffer).unwrap();
-        if n == 0 {
-            break;
-        }
+impl<'a> Searcher<'a> {
+    const BUFFER_SIZE: usize = 8192;
 
-        let result = search_subslice(&buffer, pattern);
-
-        if !result.is_empty() {
-            matches.push(result);
+    pub fn new(pattern: &'a [u8], context_bytes_size: usize) -> Self {
+        Self {
+            pattern,
+            result: Vec::new(),
+            context_bytes_size,
         }
     }
 
-    matches
-}
+    pub fn search_in_file(&mut self, filepath: &str) -> std::io::Result<()> {
+        let file = File::open(filepath)?;
 
-fn search_subslice<'a>(input: &[u8], slice: &'a [u8]) -> Vec<Match<'a>> {
-    let mut match_indexes: Vec<usize> = Vec::new();
+        let mut reader = BufReader::new(file);
+        let mut buffer = [0; Self::BUFFER_SIZE];
+        let mut pos_in_file = 0;
 
-    let mut curr_pos_pattern: usize = 0;
-    let table_of_ocurrencies = compute_toc(slice);
+        loop {
+            let n = reader.read(&mut buffer).unwrap();
 
-    for (i, &ch) in input.iter().enumerate() {
-        while curr_pos_pattern > 0 && slice[curr_pos_pattern] != ch {
-            curr_pos_pattern = table_of_ocurrencies[curr_pos_pattern - 1];
+            if n == 0 {
+                break;
+            }
+
+            let result = Self::search_slice(&buffer, self.pattern);
+
+            if !result.is_empty() {
+                // Convert the vector of indexes that match the pattern into Match objects
+                let result = result
+                    .iter()
+                    .map(|&index| {
+                        // index where we should start collecting bytes for context
+                        let offset = index - (index % self.context_bytes_size);
+
+                        // search_for_slice only return the index where the match start so we need to
+                        // create a range with all indexes from the match
+                        let match_indexes = index..index + self.pattern.len();
+
+                        // Creates the index range for the context bytes.
+                        // this can contain all the indexes for the match or partially, depends on
+                        // context_bytes_size and the pattern size
+                        let mut context_bytes_indexes = offset..offset + self.context_bytes_size;
+
+                        // In case context_bytes_size doesn't contain all of the match indexes we
+                        // need to extend the end of the range
+                        if context_bytes_indexes.end < match_indexes.end {
+                            context_bytes_indexes.end += self.context_bytes_size;
+                        }
+
+                        // The actual bytes for context + the matching bytes
+                        // only for printing the result
+                        let context_bytes = buffer[context_bytes_indexes].to_vec();
+
+                        // Now we need to know the indexes of the match inside of context_bytes
+                        let mut match_indexes = match_indexes.start % context_bytes.len()
+                            ..match_indexes.end % context_bytes.len();
+
+                        if match_indexes.end == 0 {
+                            match_indexes.end = self.context_bytes_size;
+                        }
+
+                        // The index is relative to the position in the current buffer we are
+                        // reading from the file, but we need to store the position relative to the
+                        // whole file
+                        Match::new(index + pos_in_file, match_indexes, context_bytes)
+                    })
+                    .collect();
+
+                self.result.push(result);
+            }
+            pos_in_file += Self::BUFFER_SIZE;
         }
 
-        if slice[curr_pos_pattern] == ch {
-            if curr_pos_pattern == slice.len() - 1 {
-                match_indexes.push(i - curr_pos_pattern);
-                curr_pos_pattern = table_of_ocurrencies[curr_pos_pattern];
-            } else {
-                curr_pos_pattern += 1;
+        Ok(())
+    }
+
+    /// Uses the KMP algorithm to search
+    /// Returns a vector of indexes where the slice pattern starts
+    pub fn search_slice(src: &[u8], slice: &[u8]) -> Vec<usize> {
+        let mut match_indexes: Vec<usize> = Vec::new();
+
+        let mut curr_pos_pattern: usize = 0;
+        let table_of_ocurrencies = Self::compute_toc(slice);
+
+        for (i, &ch) in src.iter().enumerate() {
+            while curr_pos_pattern > 0 && slice[curr_pos_pattern] != ch {
+                curr_pos_pattern = table_of_ocurrencies[curr_pos_pattern - 1];
+            }
+
+            if slice[curr_pos_pattern] == ch {
+                if curr_pos_pattern == slice.len() - 1 {
+                    match_indexes.push(i - curr_pos_pattern);
+                    curr_pos_pattern = table_of_ocurrencies[curr_pos_pattern];
+                } else {
+                    curr_pos_pattern += 1;
+                }
             }
         }
+
+        match_indexes
     }
 
-    match_indexes
-        .iter()
-        .map(|&index| {
-            if slice.len() > 1 {
-                Match::new(index..index + slice.len(), slice)
-            } else {
-                Match::new(index..index, slice)
+    fn compute_toc(pattern: &[u8]) -> Vec<usize> {
+        let mut table_of_ocurrencies: Vec<usize> = vec![0; pattern.len()];
+        let mut pos = 0;
+
+        for i in 1..pattern.len() {
+            while pos > 0 && pattern[i] != pattern[pos] {
+                pos = table_of_ocurrencies[pos - 1];
             }
-        })
-        .collect()
-}
 
-fn compute_toc(pattern: &[u8]) -> Vec<usize> {
-    let mut table_of_ocurrencies: Vec<usize> = vec![0; pattern.len()];
-    let mut pos = 0;
-
-    for i in 1..pattern.len() {
-        while pos > 0 && pattern[i] != pattern[pos] {
-            pos = table_of_ocurrencies[pos - 1];
+            if pattern[pos] == pattern[i] {
+                pos += 1;
+                table_of_ocurrencies[i] = pos;
+            }
         }
 
-        if pattern[pos] == pattern[i] {
-            pos += 1;
-            table_of_ocurrencies[i] = pos;
-        }
+        table_of_ocurrencies
     }
 
-    table_of_ocurrencies
+    /// Get a reference to the searcher's result.
+    pub fn result(&self) -> &[Vec<Match>] {
+        &self.result
+    }
+
+    /// Return the context bytes size.
+    pub fn context_bytes_size(&self) -> usize {
+        self.context_bytes_size
+    }
 }
 
-#[cfg(test)]
+/* #[cfg(test)]
 mod test {
     use super::*;
     #[test]
@@ -118,4 +193,4 @@ mod test {
         //     )
         // )
     }
-}
+} */
