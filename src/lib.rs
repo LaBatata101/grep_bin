791c318ab099e5d4d2a4238cc748ace9a41bfc37
LaBatata101 <labatata101@linuxmail.org>
2022-03-01 18:01:27 -0300

    use the new methods from the new version of clap and add new options:
    - print-offset: print all the offsets of the pattern

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use clap::{values_t, App, AppSettings, Arg, ArgMatches};
+use clap::{arg, command, ArgMatches};
 use std::process;
 use std::{env, path::PathBuf};
 
@@ -9,77 +9,54 @@ use utils::{file, search};
 
 use crate::utils::{print_hexdump_output, PatternType};
 
-pub fn setup_args<'a>() -> ArgMatches<'a> {
-    let integer_validator = |value: String| match value.parse::<usize>() {
+pub fn setup_args() -> ArgMatches {
+    let integer_validator = |value: &str| match value.parse::<usize>() {
         Ok(_) => Ok(()),
         Err(_) => Err(String::from("the value needs to be a valid integer")),
     };
 
-    App::new("grep_bin")
-        .version(clap::crate_version!())
-        .author(clap::crate_authors!())
-        .long_about(clap::crate_description!())
+    command!()
         .arg(
-            Arg::with_name("FILE")
-                .index(1)
-                .required(true)
-                .multiple(true)
-                .empty_values(false)
-                .help("The file path"),
-        )
-        .arg(
-            Arg::with_name("PATTERN")
-                .index(2)
-                .required(true)
-                .empty_values(false)
-                .long_help(
-                    "Can be a ascii string or a byte sequence.
 Ascii strings should be passed inside quotes like so '\"This is a string\"'
-Escaping quotes '\"This is a \\\"quote\\\"\"'
-All of these byte sequence are valid: f9b4ca, F9B4CA and f9B4Ca",
-                ),
+Escaping quotes '\"This is a \\\"quoted string\\\"\"'
+All of these byte sequence are valid: f9b4ca, F9B4CA and f9B4Ca")
         )
         .arg(
-            Arg::with_name("filetype")
-                .short("f")
-                .multiple(true)
-                .takes_value(true)
-                .empty_values(false)
-                .long_help(
-                    "Filter the search by the file extensions.
-Examples of input: jpg, mp3, exe",
-                ),
+            arg!(-f <filetype> ... "Filter the search by the file extensions.
+Examples of input: jpg, mp3, exe")
+            .required(false),
         )
         .arg(
-            Arg::with_name("context_bytes_size")
-                .short("c")
+            arg!(-c <context_bytes_size> "Defines the number of bytes that will be printed in each line.")
+                .required(false)
                 .default_value("16")
-                .validator(integer_validator)
-                .long_help("Defines the number of bytes that will be printed in each line."),
+                .validator(integer_validator),
         )
         .arg(
-            Arg::with_name("print_only")
-                .short("p")
-                .long("print-only")
-                .help("Prints only the file name that contais the match."),
+            arg!(-p --"print-only" "Prints only the filename that contais the match.")
+                .id("print_only")
+                .requires("PATTERN"),
         )
         .arg(
-            Arg::with_name("skip_bytes")
-                .short("s")
-                .long("skip-bytes")
+            arg!(-o --"print-offset" "Prints only the offsets of the match.")
+                .id("print_offset")
+                .requires("PATTERN"),
+        )
+        .arg(
+            arg!(-s --"skip-bytes" <n> "Skip n bytes before searching.")
+                .id("skip_bytes")
+                .required(false)
                 .default_value("0")
-                .takes_value(true)
-                .validator(integer_validator)
-                .help("Skip n bytes before searching."),
+                .validator(integer_validator),
         )
-        .settings(&[AppSettings::ArgRequiredElseHelp, AppSettings::ColoredHelp])
+        .arg_required_else_help(true)
         .get_matches()
 }
 
 pub fn run(args: ArgMatches) {
     let filetypes: Vec<&str> = args.values_of("filetype").unwrap_or_default().collect();
 
-    let filepaths = values_t!(args, "FILE", PathBuf).unwrap();
+    let filepaths: Vec<PathBuf> = args.values_of_t("FILE").unwrap();
     let files: Vec<PathBuf> = if filetypes.is_empty() {
         file::get_all_files_from_paths(filepaths)
     } else {
